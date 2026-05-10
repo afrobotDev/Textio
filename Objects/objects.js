633661193b3bcba65e1d9119848adf9fd6ff8cea
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-10 14:28:42 +0300

    create msgObject that has phone number, message, length of message properties

---
+++ Objects/objects.js
@@ -0,0 +1,12 @@
+function createMessage(phoneNumber, message) {
+  const msgObject = {
+    phoneNumber,
+    message,
+    messageLength : message.length 
+  };
+
+  return msgObject
+}
+
+
+export { createMessage };
