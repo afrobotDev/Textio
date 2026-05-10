cee71a0d44d509eb7b726fd20828b3794f018193
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-10 08:48:05 +0300

    getMonthlyPrice accepts tier as input and return monthly price for that tier in pennies

---
+++ functions/unit_tests.js
@@ -0,0 +1,11 @@
+export function getMonthlyPrice(tier) {
+  if (tier == "basic") {
+    return 10000;
+  } else if (tier == "premium") {
+    return 15000;
+  } else if (tier == "enterprise") {
+    return 50000;
+  } else {
+    return 0;
+  }
+}
