6184992f587b21c38d6597ac4c8ec8c7b3ff7220
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-31 17:04:45 +0300

    Add await.js with updateMessageStatus promise example

---
+++ Promises/await.js
@@ -0,0 +1,26 @@
+const promise = updateMessageStatus("M123", "Sending", true);
+const message = await promise;
+
+console.log(message);
+
+function updateMessageStatus(messageId, currentStatus, isDelivered) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (currentStatus === "Sending") {
+        if (isDelivered) {
+          resolve(
+            `Textio Message ${messageId} has been delivered successfully.`,
+          );
+        } else {
+          reject(
+            `Textio Message ${messageId} is still sending and cannot be marked as delivered.`,
+          );
+        }
+      } else {
+        resolve (
+          `Textio Message ${messageId} status updated to ${currentStatus}.`,
+        );
+      }
+    }, 1000)
+  });
+}
