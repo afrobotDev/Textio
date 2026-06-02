2db5f2e6b908f90404fecd7f9081366b31c3ff6f
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-02 13:34:48 +0300

    fix returning validation early

---
+++ TheEventLoop/task_queue.js
@@ -0,0 +1,40 @@
+function processMessages(messages) {
+  let success = true;
+  console.log(`Processing messages: ${messages}`);
+  if (messages < 0) {
+    console.log("invalid data: how do we have negative messages??");
+    success = false;
+  }
+  if (messages > 100) {
+    console.log("invalid data: way too many messages");
+    success = false;
+  }
+  finalizeJob(success, messages);
+  console.log("Doing more stuff...");
+}
+
+
+function finalizeJob(success, messages) {
+  const msg = success
+    ? `Processed ${messages} successfully!`
+    : `Failed to process messages!`;
+
+  setTimeout(() => {
+    console.log(msg);
+  },0);
+
+}
+
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+processMessages(42);
+await sleep(0);
+console.log("---");
+processMessages(-1);
+await sleep(0);
+console.log("---");
+processMessages(9001);
+
